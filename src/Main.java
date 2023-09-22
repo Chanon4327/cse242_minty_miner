476dbc56c64e61bac3ee8cedc27068afe47a9c54
administrator <111983349+jrb326@users.noreply.github.com>
2023-09-22 18:29:36 -0400

    MerkleTree and Node

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,4 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 public class Main{
-    public static void main(String args[]){
-        String hash = hashNode("Yayayada", 1234);
-        System.out.println(hash);
-    }
-
-    public static String hashNode(String addr, int bal){
-        String strBal = Integer.toString(bal);
-        String res = addr.concat(strBal);
-        
-        try{
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            BigInteger number = new BigInteger(1, md.digest(res.getBytes(StandardCharsets.UTF_8)));
-            StringBuilder hexString = new StringBuilder(number.toString(16));
-            while (hexString.length() < 64)
-            {
-                hexString.insert(0, '0');
-            }
-            return hexString.toString();
-            
-        } catch (Exception e){
-            System.out.println("Error hashing.");
-        }
-        return null;
-    }
 }
--- src/model/MerkleNode.java
+++ src/model/MerkleNode.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
 public class MerkleNode {
     private final String address;
     private final int balance;
@@ -29,8 +33,23 @@ public class MerkleNode {
         return hash;
     }
 
-    public static String toHash(String address, int balance) {
-        // todo: create sha-256 hash, of address and balance concatenated
+    public static String hashNode(String addr, int bal){
+        String strBal = Integer.toString(bal);
+        String res = addr.concat(strBal);
+
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            BigInteger number = new BigInteger(1, md.digest(res.getBytes(StandardCharsets.UTF_8)));
+            StringBuilder hexString = new StringBuilder(number.toString(16));
+            while (hexString.length() < 64)
+            {
+                hexString.insert(0, '0');
+            }
+            return hexString.toString();
+
+        } catch (Exception e){
+            System.out.println("Error hashing.");
+        }
         return null;
     }
 
