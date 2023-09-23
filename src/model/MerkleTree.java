36515de4453a02106fafff16ac0d5fbe0333e9de
cmk224 <cmk224@lehigh.edu>
2023-09-22 22:00:11 -0400

    calculate root

--- src/model/MerkleTree.java
+++ src/model/MerkleTree.java
@@ -8,9 +8,41 @@ import java.util.ArrayList;
 public class MerkleTree {
     private final ArrayList<MerkleNode> tree = new ArrayList<MerkleNode>();
 
+    public MerkleTree() {
+        merkleTree(tree);
+    }
 
     public static void calculateRoot(ArrayList<String> lines, MerkleTree tree) {
         // todo:calculate root and tree
+        System.out.println(tree);
+    }
+
+    private ArrayList<MerkleNode> merkleTree(ArrayList<MerkleNode> hashList) {
+
+        // if the root is found, return it
+        if (hashList.size() == 1) {
+            return hashList;
+        }
+
+        ArrayList<String> parentHashList = new ArrayList<String>();
+        for (int i = 0; i < hashList.size(); i+=2) {
+
+            // if only one child just use the child's hash
+            if (hashList.size() % 2 == 1) {
+                String childHash = hashList.get(i).getHash();
+                MerkleNode singleChild = new MerkleNode(childHash, i);
+                tree.add(i, singleChild);
+            }
+
+            // otherwise concat hash both and concat them
+            String stringHash = hashList.get(i).getHash().concat(hashList.get(i+1).getHash());
+            parentHashList.add(stringHash);
+            MerkleNode left = new MerkleNode(stringHash, i);
+            tree.add(i, left);
+
+        }
+
+        return merkleTree(tree); // recursive call
 
     }
 
@@ -34,11 +66,9 @@ public class MerkleTree {
 
         MerkleTree tree = new MerkleTree();
 
-
         calculateRoot(lines, tree);
 
         return tree;
-
 
     }
 }
