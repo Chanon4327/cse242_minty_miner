c3d6169f068a3ef9d8b07b1bed39758ce11a9591
skd225 <skd225@lehigh.edu>
2023-10-15 15:57:25 -0400

    "Something" for validating the blocks and the blockchain

--- code/edu/lehigh/minty/miners/model/Blockchain.java
+++ code/edu/lehigh/minty/miners/model/Blockchain.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Blockchain {
-    private final List<Block> blocks = new LinkedList<>();\
+    private final List<Block> blocks = new LinkedList<>();
 
     public Blockchain(List<String> blockFiles) {
         for (int i=0; i<blockFiles.size(); i++) {
@@ -63,5 +63,45 @@ public class Blockchain {
         
         // Address not found --> return default balance (-1)
         return -1;
+    }
+
+    /***
+     * Validates a block by getting the block's merkleRoot. Calculating the true merkle root 
+     * from the block's merkle tree and comparing the two strings.
+     * @param block block to be validated
+     * @return true if block is valid, false otherwise
+     */
+    public boolean validateBlock(Block block) {
+        String blkMerkle = block.getHeader().getMerkleRootHash();   // get the merkle root from block header
+        MerkleTree blkTree = block.getMerkleTree();                 // create merkle Tree from block's ledger
+        blkTree.calculateMerkle(blkTree.getTree());                 // calculate merkle root from block ledger
+        String trueMerkle = blkTree.getRootHash();                  // retrieve merkle root from tree
+
+        return blkMerkle == trueMerkle;
+    }
+    
+    /***
+     * Validating the entire block chain.
+     * @return true if entire blockchain is valid, false otherwise
+     */
+    public boolean validateBlockChain() {
+        for(int i = 0; i<blocks.size(); i++) {
+            Block current = blocks.get(i);
+            if(i == 0){     // root node
+                if(current.getHeader().toHash() != merkleTree.getMerkleRootHash()){
+                    return false;
+                }
+            }
+            else{   // comparing previous hash to current.previous.hash
+                Block previous = blocks.get(i-1);
+                if(previous.getHeader().toHash() != current.getPreviousHeaderHash()){       
+                    return false;
+                }
+            }
+            if(validateBlock(current) == false) {
+                return false;
+            }
+        }
+        return true;
     }
 }
